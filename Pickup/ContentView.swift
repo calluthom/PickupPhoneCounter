24aba1eeb0ffdf1ddadf13c950830193dc9fbefd
Callum Thomas <callum.thomaswork@gmail.com>
2023-01-07 10:31:03 -0800

    Add local storage, and better ui

--- Pickup/ContentView.swift
+++ Pickup/ContentView.swift
@@ -9,27 +9,50 @@ import SwiftUI
 import CoreMotion
 
 struct ContentView: View {
-    @State private var devicePickedUpCount = 0
+    
+    // get the device picked up count from local storage
+    @State private var devicePickedUpCount = UserDefaults.standard.integer(forKey: "Pickup")
+    
+    // create a motion activity manager
     let activityCenter = CMMotionActivityManager()
-
+    
+    // ui for the counter
     var body: some View {
         VStack {
-            Text("Device picked up count: \(devicePickedUpCount)")
+            Spacer()
+            Text("\(devicePickedUpCount)")
+                .font(.system(size: 200))
                 .onAppear {
                     self.startMonitoringDeviceActivity()
                 }
+            Spacer()
+            Text("You have picked up your phone \(devicePickedUpCount) times")
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.blue)
     }
-
+    
+    // monitor device activity and increment the count when the device is picked up
     func startMonitoringDeviceActivity() {
         activityCenter.startActivityUpdates(to: .main) { (activity: CMMotionActivity?) in
             if let activity = activity {
                 // Check if the device was picked up
                 if activity.stationary == false && activity.walking == false && activity.running == false && activity.automotive == false && activity.cycling == false {
+                    
                     // Device was picked up
                     self.devicePickedUpCount += 1
+                    
+                    // store the pickup count in the local device storage
+                    UserDefaults.standard.set(self.devicePickedUpCount, forKey: "Pickup")
                 }
             }
         }
+    }
+}
+
+// preview code, for easy editing in xCode
+struct ContentViewPreview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
